3afbd067e027c15c87de5b076e833e42fb2088c0
lj-n <linus.johannsen@outlook.com>
2023-01-31 20:58:39 +0100

    env

--- www/server.tsx
+++ www/server.tsx
@@ -1,50 +1,49 @@
 /** @jsx h */
 
-import * as dotenv from "https://deno.land/std@0.175.0/dotenv/mod.ts";
 import { serve } from "https://deno.land/std@0.175.0/http/server.ts";
 import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.36/mod.ts";
 
 import { getGameData, getProfileData } from "../mod.ts";
 import { Page } from "./page.tsx";
 
-const { STEAM_API_KEY } = await dotenv.load();
-console.log("FOUND ENV API KEY", STEAM_API_KEY)
+const STEAM_API_KEY = Deno.env.get("STEAM_API_KEY") as string;
+console.log("FOUND ENV API KEY", STEAM_API_KEY);
 
 function getReponse(html: string) {
-  const headers = new Headers({ "content-type": "text/html; charset=utf-8" });
-  return new Response(`<!DOCTYPE html>${html}`, { headers });
+	const headers = new Headers({ "content-type": "text/html; charset=utf-8" });
+	return new Response(`<!DOCTYPE html>${html}`, { headers });
 }
 
 async function handler(req: Request) {
-  const { searchParams, pathname } = new URL(req.url);
+	const { searchParams, pathname } = new URL(req.url);
 
-  if (pathname.startsWith("/robots.txt")) {
-    const file = await Deno.readFile("www/robots.txt");
-    return new Response(file, {
-      headers: { "content-type": "text/plain" },
-    });
-  }
+	if (pathname.startsWith("/robots.txt")) {
+		const file = await Deno.readFile("www/robots.txt");
+		return new Response(file, {
+			headers: { "content-type": "text/plain" },
+		});
+	}
 
-  /** User Input */
-  const steamId = searchParams.get("steamId");
-  const apiKey = searchParams.get("apiKey") || STEAM_API_KEY;
+	/** User Input */
+	const steamId = searchParams.get("steamId");
+	const apiKey = searchParams.get("apiKey") || STEAM_API_KEY;
 
-  if (!steamId) {
-    return getReponse(renderSSR(<Page />));
-  }
+	if (!steamId) {
+		return getReponse(renderSSR(<Page />));
+	}
 
-  try {
-    const [profile, games] = await Promise.all([
-      getProfileData({ steamId, apiKey }),
-      getGameData({ steamId, apiKey }),
-    ]);
+	try {
+		const [profile, games] = await Promise.all([
+			getProfileData({ steamId, apiKey }),
+			getGameData({ steamId, apiKey }),
+		]);
 
-    return getReponse(renderSSR(<Page profile={profile} games={games} />));
-  } catch (error) {
-    console.log(error);
-    const message = "Error! Be sure to use a valid ID and/or Key!";
-    return getReponse(renderSSR(<Page error={message} />));
-  }
+		return getReponse(renderSSR(<Page profile={profile} games={games} />));
+	} catch (error) {
+		console.log(error);
+		const message = "Error! Be sure to use a valid ID and/or Key!";
+		return getReponse(renderSSR(<Page error={message} />));
+	}
 }
 
 serve(handler);
