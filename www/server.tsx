96294bb0de7ce984801a6985e64a5198f4f0d041
lj-n <linus.johannsen@outlook.com>
2023-01-31 20:59:34 +0100

    formatting

--- www/server.tsx
+++ www/server.tsx
@@ -9,40 +9,40 @@ import { Page } from "./page.tsx";
 const STEAM_API_KEY = Deno.env.get("STEAM_API_KEY") as string;
 
 function getReponse(html: string) {
-	const headers = new Headers({ "content-type": "text/html; charset=utf-8" });
-	return new Response(`<!DOCTYPE html>${html}`, { headers });
+  const headers = new Headers({ "content-type": "text/html; charset=utf-8" });
+  return new Response(`<!DOCTYPE html>${html}`, { headers });
 }
 
 async function handler(req: Request) {
-	const { searchParams, pathname } = new URL(req.url);
+  const { searchParams, pathname } = new URL(req.url);
 
-	if (pathname.startsWith("/robots.txt")) {
-		const file = await Deno.readFile("www/robots.txt");
-		return new Response(file, {
-			headers: { "content-type": "text/plain" },
-		});
-	}
+  if (pathname.startsWith("/robots.txt")) {
+    const file = await Deno.readFile("www/robots.txt");
+    return new Response(file, {
+      headers: { "content-type": "text/plain" },
+    });
+  }
 
-	/** User Input */
-	const steamId = searchParams.get("steamId");
-	const apiKey = searchParams.get("apiKey") || STEAM_API_KEY;
+  /** User Input */
+  const steamId = searchParams.get("steamId");
+  const apiKey = searchParams.get("apiKey") || STEAM_API_KEY;
 
-	if (!steamId) {
-		return getReponse(renderSSR(<Page />));
-	}
+  if (!steamId) {
+    return getReponse(renderSSR(<Page />));
+  }
 
-	try {
-		const [profile, games] = await Promise.all([
-			getProfileData({ steamId, apiKey }),
-			getGameData({ steamId, apiKey }),
-		]);
+  try {
+    const [profile, games] = await Promise.all([
+      getProfileData({ steamId, apiKey }),
+      getGameData({ steamId, apiKey }),
+    ]);
 
-		return getReponse(renderSSR(<Page profile={profile} games={games} />));
-	} catch (error) {
-		console.log(error);
-		const message = "Error! Be sure to use a valid ID and/or Key!";
-		return getReponse(renderSSR(<Page error={message} />));
-	}
+    return getReponse(renderSSR(<Page profile={profile} games={games} />));
+  } catch (error) {
+    console.log(error);
+    const message = "Error! Be sure to use a valid ID and/or Key!";
+    return getReponse(renderSSR(<Page error={message} />));
+  }
 }
 
 serve(handler);
