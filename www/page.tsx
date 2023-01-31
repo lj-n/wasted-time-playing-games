abf7c36ced7bdea5001084b384b10b4defb966c2
lj-n <linus.johannsen@outlook.com>
2023-01-31 18:16:37 +0100

    better link

--- www/page.tsx
+++ www/page.tsx
@@ -7,9 +7,9 @@
 /// <reference lib="deno.ns" />
 
 import {
-  Fragment,
-  h,
-  render,
+	Fragment,
+	h,
+	render,
 } from "https://deno.land/x/nano_jsx@v0.0.36/mod.ts";
 import { Language, minify } from "https://deno.land/x/minifier@v1.1.1/mod.ts";
 import type { GameData, ProfileData } from "../mod.ts";
@@ -17,114 +17,115 @@ import type { GameData, ProfileData } from "../mod.ts";
 const css = await Deno.readTextFile("www/style.css");
 
 interface PageProps {
-  profile?: ProfileData;
-  games?: GameData;
-  error?: string;
+	profile?: ProfileData;
+	games?: GameData;
+	error?: string;
 }
 
 const formatMinutes = (m: number) => {
-  const h = Math.floor(m / 60);
-  m = m % 60;
+	const h = Math.floor(m / 60);
+	m = m % 60;
 
-  const hours = h === 1 ? "1 hour" : h > 1 ? `${h} hours` : "";
-  const minutes = m === 1 ? "1 minute" : m > 1 ? `${m} minutes` : "";
+	const hours = h === 1 ? "1 hour" : h > 1 ? `${h} hours` : "";
+	const minutes = m === 1 ? "1 minute" : m > 1 ? `${m} minutes` : "";
 
-  return `${hours} ${minutes}`.trim();
+	return `${hours} ${minutes}`.trim();
 };
 
 export function Page({ error, games, profile }: PageProps) {
-  const Form = render(
-    <form action="/">
-      <p class="info">
-        If you provide no Steam web api key, the user profile must be public!
-        You can create an api key{" "}
-        <a href="https://steamcommunity.com/dev/apikey" title="Go to the steam web api key site">here</a>.
-      </p>
-      <label>
-        Steam web api key
-        <input type="text" name="apiKey" id="apy-key" />
-      </label>
-      <label>
-        Steam id
-        <input
-          type="text"
-          name="steamId"
-          id="steam-id"
-          palceholder="steam user id"
-        />
-      </label>
+	const Form = render(
+		<form action="/">
+			<p class="info">
+				If you provide no Steam web api key, the user profile must be
+				public! You can{" "}
+				<a href="https://steamcommunity.com/dev/apikey">
+					create an api key here
+				</a>
+				.
+			</p>
+			<label>
+				Steam web api key
+				<input type="text" name="apiKey" id="apy-key" />
+			</label>
+			<label>
+				Steam id
+				<input
+					type="text"
+					name="steamId"
+					id="steam-id"
+					palceholder="steam user id"
+				/>
+			</label>
 
-      <button type="submit">sum my hours</button>
-    </form>,
-  );
+			<button type="submit">sum my hours</button>
+		</form>
+	);
 
-  const Content = render(
-    games && profile
-      ? (
-        <>
-          <div class="divider" />
-          <div class="profile">
-            <img
-              src={profile.avatar}
-              alt={`${profile.profileName} Steam Avatar`}
-              height="64px"
-              width="64px"
-            />
-            <div>
-              <h2>{profile.profileName}</h2>
-              <span>Steam id: {profile.steamId}</span>
-            </div>
-          </div>
-          <span class="total">
-            📜 You played a total of{" "}
-            <strong>{formatMinutes(games.totalPlaytime)}</strong>!
-          </span>
-          Your top played games are:
-          <table>
-            {games.mostPlayed.map((g) => (
-              <tr>
-                <td>{g.name}:</td>
-                <td>{formatMinutes(g.playtime)}</td>
-              </tr>
-            ))}
-          </table>
-        </>
-      )
-      : null,
-  );
+	const Content = render(
+		games && profile ? (
+			<>
+				<div class="divider" />
+				<div class="profile">
+					<img
+						src={profile.avatar}
+						alt={`${profile.profileName} Steam Avatar`}
+						height="64px"
+						width="64px"
+					/>
+					<div>
+						<h2>{profile.profileName}</h2>
+						<span>Steam id: {profile.steamId}</span>
+					</div>
+				</div>
+				<span class="total">
+					📜 You played a total of{" "}
+					<strong>{formatMinutes(games.totalPlaytime)}</strong>!
+				</span>
+				Your top played games are:
+				<table>
+					{games.mostPlayed.map((g) => (
+						<tr>
+							<td>{g.name}:</td>
+							<td>{formatMinutes(g.playtime)}</td>
+						</tr>
+					))}
+				</table>
+			</>
+		) : null
+	);
 
-  return (
-    <html lang="en">
-      <head>
-        <title>Get Your Total Played Steam Hours</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width" />
-        <meta
-          name="description"
-          content="Calculate the amount of time you wasted playing Steam games."
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Get Your Total Played Steam Hours"
-        />
-        <meta
-          property="og:description"
-          content="Calculate the amount of time you wasted playing Steam games."
-        />
-        <style>{minify(Language.CSS, css)}</style>
-      </head>
-      <body>
-        <h1>Get Your Total Played Steam Hours</h1>
+	return (
+		<html lang="en">
+			<head>
+				<title>Get Your Total Played Steam Hours</title>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width" />
+				<meta
+					name="description"
+					content="Calculate the amount of time you wasted playing Steam games."
+				/>
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:title"
+					content="Get Your Total Played Steam Hours"
+				/>
+				<meta
+					property="og:description"
+					content="Calculate the amount of time you wasted playing Steam games."
+				/>
+				<style>{minify(Language.CSS, css)}</style>
+			</head>
+			<body>
+				<h1>Get Your Total Played Steam Hours</h1>
 
-        <Content />
+				<Content />
 
-        <div class="divider" />
+				<div class="divider" />
 
-        <Form />
+				<Form />
 
-        {error ? <span class="error">{error}</span> : null}
-      </body>
-    </html>
-  );
+				{error ? <span class="error">{error}</span> : null}
+			</body>
+		</html>
+	);
 }
