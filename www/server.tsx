d3e017dfa2df7f45ef59afd5b0e1f4f32a2afd95
lj-n <linus.johannsen@outlook.com>
2023-01-31 17:49:29 +0100

    fix deno env variable

--- www/server.tsx
+++ www/server.tsx
@@ -1,14 +1,13 @@
 /** @jsx h */
 
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-
+import * as dotenv from "https://deno.land/std@0.175.0/dotenv/mod.ts";
 import { serve } from "https://deno.land/std@0.175.0/http/server.ts";
 import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.36/mod.ts";
 
 import { getGameData, getProfileData } from "../mod.ts";
 import { Page } from "./page.tsx";
 
-const { STEAM_API_KEY } = config({ path: "www/.env" });
+const { STEAM_API_KEY } = await dotenv.load({ envPath: "www/.env" });
 
 async function handler(req: Request) {
   /** User Input */
